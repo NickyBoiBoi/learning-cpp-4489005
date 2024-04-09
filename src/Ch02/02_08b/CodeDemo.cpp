a5367fe24bc616deabdb0f3cf5bdec1023bab12a
NickyBoiBoi <134409436+NickyBoiBoi@users.noreply.github.com>
2024-04-09 15:31:23 +0000

    Completed Chapter 2.8

--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -4,8 +4,28 @@
 
 #include <iostream>
 
+//#define AGE_LENGTH 4
+
 int main(){
-    
+    const size_t AGE_LENGTH = 4;
+
+    int age[AGE_LENGTH];
+    float temperature[] = {31.5, 32.7, 38.9};
+
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+
+    std::cout << "The Age Array has " << AGE_LENGTH << " Elements" << std::endl;
+    std::cout << "age[0] = " << age[0] << std::endl;
+    std::cout << "age[1] = " << age[1] << std::endl;
+    std::cout << "age[2] = " << age[2] << std::endl;
+    std::cout << "age[3] = " << age[3] << std::endl;
+    std::cout << std::endl << std::endl;
+    std::cout << "Temp[0] = " << temperature[0] << std::endl;
+    std::cout << "Temp[1] = " << temperature[1] << std::endl;
+    std::cout << "Temp[2] = " << temperature[2] << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
